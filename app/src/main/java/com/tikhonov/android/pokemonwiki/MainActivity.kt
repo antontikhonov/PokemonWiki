d614d0a11528a1ff29ad0950b5b594bf971a3725
Anton Tikhonov <free.tikhonov@gmail.com>
2020-10-17 22:40:50 +0400

    add SearchActivity

--- app/src/main/java/com/tikhonov/android/pokemonwiki/MainActivity.kt
+++ app/src/main/java/com/tikhonov/android/pokemonwiki/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.tikhonov.android.pokemonwiki
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toSearchActivity(view : View) {
+        val intent = Intent(this, SearchActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/tikhonov/android/pokemonwiki/SearchActivity.kt
@@ -0,0 +1,11 @@
+package com.tikhonov.android.pokemonwiki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
